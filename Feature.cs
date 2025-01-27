9da74c37ed9d7f657d00f4a66fef6c60a05e608a
palakSingh621 <kushwahps621@gmail.com>
2025-01-27 16:22:01 +0530

    Task-3 Reversing the number

--- Feature.cs
+++ Feature.cs
@@ -1,10 +1,22 @@
 using System;
 class Review1
 {
-	pulic static void Main(String[] args)
+	public static void Main(String[] args)
 	{
 		int a= 123456;
-		
+		Review1 obj=new Review1();
+		int reverse= obj.Reverse(a);
+		Console.WriteLine("Task-3 Reversing the number: "+reverse);
 	}
-	
+	public int Reverse(int n)
+	{
+		int rev=0;
+		while(n!=0)
+		{
+			int d=n%10;
+			rev=rev*10+d;
+			n=n/10;
+		}
+		return rev;
+	}
 }
